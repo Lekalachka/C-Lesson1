27f79986769741f3ea03e60cbb12039ec1e5d154
Elena <lekala@yandex.ru>
2022-11-04 22:27:42 +0300

    add target 21

--- target3/Program.cs
+++ target3/Program.cs
@@ -3,17 +3,19 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 // 
-Console.WriteLine("Введите координату х точки а");
-int ax = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату y точки а");
-int ay = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату z точки а");
-int az = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату х точки b");
-int bx = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату y точки b");
-int by = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату z точки b");
-int bz = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату х точки а");
+// int ax = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату y точки а");
+// int ay = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату z точки а");
+// int az = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату х точки b");
+// int bx = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату y точки b");
+// int by = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату z точки b");
+// int bz = Convert.ToInt32(Console.ReadLine());
 
-Double dist = Math.s
+// Double dist = Math.Sqrt(Math.Pow(bx-ax,2)+Math.Pow(by-ay,2)+Math.Pow(bz-az,2));
+// Console.WriteLine($"Расстояние между двумя точками равно {dist}");
+
