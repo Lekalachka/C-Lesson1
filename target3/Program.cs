f7293ec520e6df0aa882097475ea979086d95ec7
Elena <lekala@yandex.ru>
2022-11-05 18:40:36 +0300

    add info

--- target3/Program.cs
+++ target3/Program.cs
@@ -24,12 +24,49 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-Console.WriteLine("Введите число");
+// Console.WriteLine("Введите число");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+//     while (count <=x)
+//     {
+//         Console.Write($"{count*count*count}, ");
+//         count++;
+//     }
+
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом. Через строку решать нельзя.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
 int x = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-    while (count <=x)
+int a = x / 10000;
+int b = x % 10;
+int c = ((x / 1000) % 10);
+int d = ((x % 100) / 10);
+int razryad = 0;
+
+void CountNumbers(int x)
+{
+    int razryad = 0;
+    while (x % 10 > 0)
     {
-        Console.Write($"{count*count*count}, ");
-        count++;
+        razryad++;
+        x /= 10;
     }
-    
+    Console.WriteLine(razryad);
+}
+
+if (razryad < 5 || razryad>5)
+{
+    if (a == b && c == d)
+    {
+        Console.WriteLine("да");
+    }
+    else Console.WriteLine("нет");
+}
+else Console.WriteLine("вы ввели недопустимое число");
+
+
